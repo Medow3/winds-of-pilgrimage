e79b3725bfd9880a9ae6254c800cc70bf9131d23
Grace Schorno <graceschorno@protonmail.com>
2023-09-29 20:54:08 -0700

    Finish button

--- src/puzzle/button.gd
+++ src/puzzle/button.gd
@@ -1,21 +1,48 @@
-extends StaticBody3D
+extends MeshInstance3D
 
-@export var is_pressed = true
+@export var is_pressed = false : set = _set_is_pressed
+@export var is_disabled = false : set = _set_is_disabled
+@export var disable_on_toggle = false
+
+@export var pressed_texture: StandardMaterial3D = null;
+@export var released_texture: StandardMaterial3D = null;
+@export var disabled_texture: StandardMaterial3D = null;
 
 signal pressed();
-
 signal released();
+signal disabled();
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass 
-	
-func _on_area_3d_body_entered(body):
-	is_pressed = !is_pressed
-	if is_pressed:
-		emit_signal("pressed")
-	else:
-		emit_signal("released")
+func _set_is_pressed(val: bool):
+	is_pressed = val
+	_set_texture()
 
-func _on_area_3d_body_exited(body):
-	pass
+func _set_is_disabled(val: bool):
+	is_disabled = val
+	_set_texture()
+
+func _set_texture():
+	if disabled and disabled_texture != null:
+		set_surface_override_material(0, disabled_texture)
+	elif is_pressed:
+		if pressed_texture != null:
+			set_surface_override_material(0, pressed_texture)
+	elif released_texture != null:
+		set_surface_override_material(0, released_texture)
+
+
+func _on_trigger_body_entered(body):
+	if body is Player:
+		if is_disabled:
+			return
+			
+		is_pressed = !is_pressed
+
+		if disable_on_toggle:
+			is_disabled = true
+		
+
+func _on_trigger_body_exited(body):
+	pass # Replace with function body.
+
+
+
